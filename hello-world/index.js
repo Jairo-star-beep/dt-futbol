f2aa8adaf3ac4fda27cc79bada7bcc9a20d4be83
Jairo-star-beep <jairo.coppe@anima.edu.uy>
2024-05-08 09:22:00 -0300

    Modificacion con codigos https

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,2 +1,10 @@
-const express = require("express");
+const express = require('express');
+const jugador = require('../3-jugador/jugador')
 const app = express();
+
+app.get('/', (req, res) => res.send('Hello Mundo!'));
+
+app.get('/jugadores', (req, res) => res.status(202).send(jugador.lala()));
+
+
+app.listen(3000, () => (console.log('Server running on port 3000')));
